0d6a5d008c59517a95778603d2e103150b1330ed
liberodark <liberodark@gmail.com>
2026-05-23 00:34:44 +0200

    bananapi/bpi-f3: init

---
+++ bananapi/bpi-sm10/default.nix
@@ -0,0 +1,16 @@
+{
+  lib,
+  ...
+}:
+
+{
+  # The BPI-SM10 (K3-CoM260) shares the same SoC, firmware and boot flow as the
+  # K3 Pico-ITX. Only the device tree differs (different carrier board).
+  #
+  # The board's firmware reports product_name = "k3_com260_ifx" (read from the
+  # SpacemiT EFI variable 4F556D5E-CF38-CDF8-229C-0178D00FF043:product_name),
+  # which selects k3_com260_ifx.dtb.
+  imports = [ ../../spacemit/k3-pico-itx ];
+
+  hardware.deviceTree.name = lib.mkForce "spacemit/k3_com260_ifx.dtb";
+}
